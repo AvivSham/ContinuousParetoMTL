4650b73953f6aa596b9c0f591a2675fccdfef397
AvivSham <Mista2311@gmail.com>
2020-09-21 15:57:35 +0300

    integrating HN

--- multi_mnist/cpmtl.py
+++ multi_mnist/cpmtl.py
@@ -1,10 +1,14 @@
 import random
+import argparse
 from pathlib import Path
 from termcolor import colored
 
 import numpy as np
+import pickle
+from sklearn.model_selection import train_test_split
 
 import torch
+import torch.nn as nn
 import torch.nn.functional as F
 from torch.optim import SGD
 from torchvision import transforms
@@ -14,6 +18,75 @@ from pareto.optim import VisionHVPSolver, MINRESKKTSolver
 from pareto.datasets import MultiMNIST
 from pareto.networks import MultiLeNet
 from pareto.utils import TopTrace
+
+class LeNet(torch.nn.Module):
+    def __init__(self, n_tasks):
+        super(LeNet, self).__init__()
+        self.n_tasks = n_tasks
+        self.conv1 = nn.Conv2d(1, 10, 9, 1)
+        self.conv2 = nn.Conv2d(10, 20, 5, 1)
+        self.fc1 = nn.Linear(5 * 5 * 20, 50)
+
+        for i in range(self.n_tasks):
+            setattr(self, 'task_{}'.format(i), nn.Linear(50, 10))
+
+    def shared_parameters(self):
+        return list([p for n, p in self.named_parameters() if 'task' not in n])
+
+    def forward(self, x, i=None):
+        x = F.relu(self.conv1(x))
+        x = F.max_pool2d(x, 2, 2)
+        x = F.relu(self.conv2(x))
+        x = F.max_pool2d(x, 2, 2)
+        x = x.view(-1, 5 * 5 * 20)
+        x = F.relu(self.fc1(x))
+
+        if i is not None:
+            layer_i = getattr(self, 'task_{}'.format(i))
+            return layer_i(x)
+
+        outs = []
+        for i in range(self.n_tasks):
+            layer = getattr(self, 'task_{}'.format(i))
+            outs.append(layer(x))
+
+        return torch.stack(outs, dim=0)
+
+
+class Dataset:
+
+    def __init__(self, path, val_size=0.):
+        self.path = path
+        self.val_size = val_size
+
+    def get_datasets(self):
+        with open(self.path, 'rb') as f:
+            trainX, trainLabel, testX, testLabel = pickle.load(f)
+
+        n_train = len(trainX)
+        if self.val_size > 0:
+            trainX, valX, trainLabel, valLabel = train_test_split(
+                trainX, trainLabel, test_size=self.val_size, random_state=42
+            )
+            n_train = len(trainX)
+            n_val = len(valX)
+
+        trainX = torch.from_numpy(trainX.reshape(n_train, 1, 36, 36)).float()
+        trainLabel = torch.from_numpy(trainLabel).long()
+        testX = torch.from_numpy(testX.reshape(20000, 1, 36, 36)).float()
+        testLabel = torch.from_numpy(testLabel).long()
+
+        train_set = torch.utils.data.TensorDataset(trainX, trainLabel)
+        test_set = torch.utils.data.TensorDataset(testX, testLabel)
+
+        if self.val_size > 0:
+            valX = torch.from_numpy(valX.reshape(n_val, 1, 36, 36)).float()
+            valLabel = torch.from_numpy(valLabel).long()
+            val_set = torch.utils.data.TensorDataset(valX, valLabel)
+
+            return train_set, val_set, test_set
+
+        return train_set, test_set
 
 
 @torch.no_grad()
@@ -46,7 +119,7 @@ def evaluate(network, dataloader, device, closures, header=''):
     return losses, top1s
 
 
-def train(start_path, beta):
+def train(start_path, data_path, beta, lr, batch_size, num_steps):
 
     # prepare hyper-parameters
 
@@ -55,7 +128,6 @@ def train(start_path, beta):
     cuda_enabled = True
     cuda_deterministic = False
 
-    batch_size = 2048
     num_workers = 2
 
     shared = False
@@ -69,11 +141,9 @@ def train(start_path, beta):
     damping = 0.1
     maxiter = 50
 
-    lr = 0.1
+    # lr = lr
     momentum = 0.0
     weight_decay = 0.0
-
-    num_steps = 10
 
     verbose = False
 
@@ -118,18 +188,20 @@ def train(start_path, beta):
 
     # prepare dataset
 
-    transform = transforms.Compose([transforms.ToTensor(), transforms.Normalize((0.1307,), (0.3081,))])
+    # transform = transforms.Compose([transforms.ToTensor(), transforms.Normalize((0.1307,), (0.3081,))])
+    #
+    # trainset = MultiMNIST(dataset_path, train=True, download=True, transform=transform)
+    #
+    # testset = MultiMNIST(dataset_path, train=False, download=True, transform=transform)
 
-    trainset = MultiMNIST(dataset_path, train=True, download=True, transform=transform)
+    trainset, _, testset = Dataset(data_path, val_size=0.1).get_datasets()
     trainloader = torch.utils.data.DataLoader(trainset, batch_size=batch_size, shuffle=True, num_workers=num_workers)
-
-    testset = MultiMNIST(dataset_path, train=False, download=True, transform=transform)
     testloader = torch.utils.data.DataLoader(testset, batch_size=batch_size, shuffle=False, num_workers=num_workers)
 
 
     # prepare network
 
-    network = MultiLeNet()
+    network = LeNet(n_tasks=2)
     network.to(device)
 
 
@@ -162,7 +234,7 @@ def train(start_path, beta):
 
     # prepare optimizer
 
-    optimizer = SGD(network.parameters(), lr=lr, momentum=momentum, weight_decay=weight_decay)
+    optimizer = torch.optim.Adam(network.parameters(), lr=lr)
 
 
     # first evaluation
@@ -202,15 +274,34 @@ def train(start_path, beta):
     hvp_solver.close()
 
 
-def cpmtl():
-    root_path = Path(__file__).resolve().parent
-    start_root = root_path / 'weighted_sum'
-
+def cpmtl(ckpt_path, data_path, lr, bs, n_steps):
     beta = torch.Tensor([1, 0])
 
-    for start_path in sorted(start_root.glob('[0-9]*.pth'), key=lambda x: int(x.name.split('.')[0])):
-        train(start_path, beta)
+    for start_path in sorted(Path(ckpt_path).glob('*.pth'), key=lambda x: int(x.name.split('.')[0])):
+        train(start_path, data_path, beta, lr, bs, n_steps)
 
 
 if __name__ == "__main__":
-    cpmtl()
+    parser = argparse.ArgumentParser(description="continuous PMTL MultiMNIST")
+    parser.add_argument('--num-steps', type=int, default=10, help='num steps for expansion')
+    parser.add_argument('--lr', type=float, default=1e-4, help='learning rate')
+    parser.add_argument('--batch-size', type=int, default=512, help='batch size')
+    parser.add_argument('--data-path', type=str,
+                        default='/cortex/data/images/paretoHN/data/multi_mnist.pickle', help='path for data')
+    parser.add_argument('--ckpt-path', type=str,
+                        default='/cortex/data/images/paretoHN/data/model', help='path for start solutions')
+
+    args = parser.parse_args()
+
+    ckpt_path = args.ckpt_path
+    data_path = args.data_path
+    lr = args.lr
+    bs = args.batch_size
+    niter = args.num_steps
+    cpmtl(
+        ckpt_path=ckpt_path,
+        data_path=data_path,
+        lr=lr,
+        bs=bs,
+        n_steps=niter
+    )
